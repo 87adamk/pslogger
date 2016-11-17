f43b98ff7dde237f7c6e3b0275054ab9f9e0b7c2
OPENSNS 김수민 <smkim87@opensns.co.kr>
2016-11-17 15:23:52 +0900

    npm body-parsor-json 추가

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
 app.use(express.static(__dirname + '/public'));
 
