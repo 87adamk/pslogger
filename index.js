889fb0c3ee2330ffe694463cfef69f3242af8169
OPENSNS 김수민 <smkim87@opensns.co.kr>
2016-11-17 15:26:59 +0900

    no message

--- index.js
+++ index.js
@@ -4,16 +4,14 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use( bodyParser.json() );
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-}));
-
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', function(request, response) {
   response.render('pages/index');
