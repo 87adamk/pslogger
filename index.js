00d35691bfe8fb679b150816d56bbff0ef504efc
OPENSNS 김수민 <smkim87@opensns.co.kr>
2016-11-17 15:19:05 +0900

    no message

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.post('/send', function(request, response) {
+	console.log(request.body.message);
+  response.render('pages/index');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
